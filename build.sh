1e5d4fbc3084a51457c7a4b871efcb618612ceab
Terry Morse <tmorse@terrymorse.com>
2020-02-04 14:36:16 -0800

    default to minified version

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# build deep-copy-all for distribution
-#echo -n "Making deep-copy-all index.js file with babel..."
-#babel index.mjs -o index.js --plugins=@babel/plugin-transform-modules-commonjs
-#echo "babel done."
-
 echo "Building minified source files:"
 gulp --gulpfile gulpfile.js
 echo "Minify complete."
--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,8 @@ const gulp = require("gulp");
 const minify = require("gulp-minify");
 const rename = require("gulp-rename");
 
-const MJS_FILE = 'index.mjs';
 const JS_FILE = 'index.js';
 const DEST_DIR = './min';
-
-gulp.task('minifymjs', () => {
-  console.log(`Minifying ${MJS_FILE}...`);
-  return gulp.src(MJS_FILE, { allowEmpty: true })
-    .pipe(minify({noSource: true}))
-    .pipe(rename(path => {
-      path.basename = path.basename.replace(/-min/,'.min');
-      return path;
-    }))
-    .pipe(gulp.dest(DEST_DIR))
-});
 
 gulp.task('minifyjs', () => {
   console.log(`Minifying ${JS_FILE}...`);
@@ -28,4 +16,4 @@ gulp.task('minifyjs', () => {
     .pipe(gulp.dest(DEST_DIR))
 });
 
-gulp.task('default', gulp.series(['minifymjs','minifyjs']));
+gulp.task('default', gulp.series(['minifyjs']));
